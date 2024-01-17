0a967ce630784d1a109cb665955a5917c8dc091b
KishoreReddy-45 <155902613+KishoreReddy-45@users.noreply.github.com>
2024-01-18 02:54:08 +0530

    Update file_operation.py

--- file_operation.py
+++ file_operation.py
@@ -1,3 +1,5 @@
+#file operation
+
 def update_server_config(file_path, key, value):
   # create a file
   with open(file_path, "r") as file:
@@ -6,9 +8,9 @@ def update_server_config(file_path, key, value):
   # create a file in update mode
   with open(file_path, "w") as file:
     for line in lines:
-       if key in line:
-        file.write(key + "=" + value + "\n")
-    else:
-        file.write(line)
+        if key in line:
+          file.write(key + "=" + value + "\n")
+        else:
+          file.write(line)
       
 update_server_config("server.config","MAX_CONNECTIONS","100")
